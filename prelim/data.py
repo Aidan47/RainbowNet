56a47fd11d6222d892f84a0ac041727c3da0d121
Aidan R <aidanrolle2@gmail.com>
2024-08-13 20:24:42 -0400

    Collect OBS states and corresponding game

--- prelim/data.py
+++ prelim/data.py
@@ -1,5 +1,29 @@
 import gymnasium as gym
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+# can a cnn identify the game based on obs state
+
+def collect(game, trials=320):
+    data = []
+    
+    env = gym.make(game)
+    while trials > 0:
+        obs, terminated, truncated = env.reset(), False, False
+        while not terminated and not truncated:
+            action = env.action_space.sample()
+            obs, reward, terminated, truncated, _ = env.step(action)
+            data.append((obs, game))
+        trials -= 1
+    return np.array(data, dtype=object)
+
+if __name__ == "__main__":
+    games = ["Humanoid-v4", "HumanoidStandup-v4", "Hopper-v4"]
+
+    data = []
+    for game in games:
+        data.extend(collect(game))
+        print(f"{game} collected")
+        print(f"Total data points: {len(data)}")
+    np.save('gameData', data)

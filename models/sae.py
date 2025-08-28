63a16c0e25e2e512c88de2be07c6f60244538387
Aidan R <aidanrolle2@gmail.com>
2025-08-28 04:25:52 -0400

    implement plain SAE with multiple regularization methods

---
+++ models/sae.py
@@ -0,0 +1,70 @@
+from math import log2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from .encoder import Encoder
+from .decoder import Decoder
+
+
+class SAE(nn.Module):
+    def __init__(self, input: int, latent_factor: int, output: int):
+        super(SAE, self).__init__()
+        
+        # get layer dimensions
+        encoder_H = pow(2, int(log2(input)) + 1)
+        latent = latent_factor * encoder_H
+        
+        # make models
+        self.encoder = Encoder(input, encoder_H, latent)
+        self.decoder = Decoder(latent, output)
+
+        self.z = torch.Tensor([])
+        
+    def type(self, x):
+        if type(x) is not torch.Tensor:
+            x = torch.from_numpy(x).float()
+        return x
+    
+    def topk(self, x, k=64):
+        top_k_values, _ = torch.topk(x, k)
+        kth_value = top_k_values[:, -1].unsqueeze(-1)
+        # a mask to zero out non-top-k values
+        mask = (x >= kth_value).float()
+        return x * mask
+        
+    
+    def forward(self, x, **kwargs):
+        topk = kwargs.get("topk", True)
+        x = self.type(x)
+        x = self.z = self.encoder.forward(x)
+        if topk:
+            x = self.z = self.topk(x)
+        x = self.decoder.forward(x)
+        return x
+    
+    def encode(self):
+        # forward without decoder
+        pass
+    
+    def l1_reg(self):
+        return torch.sum(torch.abs(self.z))
+    
+    def l2_reg(self):
+        W2 = torch.square(self.decoder.l1.weight)
+        return W2.sum()
+    
+    def kl_penalty(self, target_sparsity):
+        rho_hat = torch.mean(self.z)  # average activation per unit
+        print(rho_hat.shape)
+        rho = torch.full_like(rho_hat, target_sparsity)
+        
+        term1 = rho * torch.log(rho / rho_hat.clamp(min=1e-8))
+        term2 = (1 - rho) * torch.log((1 - rho) / (1 - rho_hat).clamp(min=1e-8))
+        kl_div = term1 + term2
+        
+        return torch.sum(kl_div)
+    
+    def save(self):
+        torch.save(self.encoder.state_dict(), "checkpoints/sae/encoder.pth")
+        torch.save(self.decoder.state_dict(), "checkpoints/sae/decoder.pth")        
